91f86d9fca99030807ab6b5966ea05daced2e1ad
Craig Franceschetti <craigfranceschetti@gmail.com>
2021-09-28 18:15:47 -0500

    Add project files.

---
+++ DND_DiceRoller_App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DND_DiceRoller_Library;
+
+namespace DND_DiceRoller_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DiceRoller.RollD20());
+            Console.ReadKey();
+        }   
+    }
+
+
+    
+}
