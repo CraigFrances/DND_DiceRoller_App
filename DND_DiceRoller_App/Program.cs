4c56daf9a20a7c3867470be166a1a6b78479cf6f
Craig Franceschetti <craigfranceschetti@gmail.com>
2021-09-28 20:13:34 -0500

    Working roll mechanic

--- DND_DiceRoller_App/Program.cs
+++ DND_DiceRoller_App/Program.cs
@@ -9,12 +9,50 @@ namespace DND_DiceRoller_App
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            //Roll is a static method
-            Console.WriteLine(DiceRoller.RollD20());
+           
+
+            //Make into method
+            Console.WriteLine("Greetings, Welcome to the dice roller.");
+            //Method
+            Console.WriteLine("Which dice do you want to roll?");
+            Console.WriteLine("Your choices are: D4, D6, D8, D10, D12, D20");
+            Console.Write("Type your selection: ");
+
+            var diceSelection = Console.ReadLine().ToUpper();
+            Console.WriteLine($"Your Roll: {DiceRoll(diceSelection)}");
             Console.ReadKey();
-        }   
+        }
+
+        private static int DiceRoll(string diceSelection)
+        {
+            int roll = 0;
+            switch (diceSelection)
+            {
+                case "D4":
+                    roll = DiceRoller.RollD4();
+                    break;
+                case "D6":
+                    roll = DiceRoller.RollD6();
+                    break;
+                case "D8":
+                    roll = DiceRoller.RollD8();
+                    break;
+                case "D10":
+                    roll = DiceRoller.RollD10();
+                    break;
+                case "D12":
+                    roll = DiceRoller.RollD12();
+                    break;
+                case "D20":
+                    roll = DiceRoller.RollD20();
+                    break;
+
+            }
+            return roll;
+        }
     }
 
 
